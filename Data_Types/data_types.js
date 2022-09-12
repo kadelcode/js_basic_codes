887dfc07b9bdb9c23057c5eaa2a6bf6bba85a114
kadelcode <kadelcode@gmail.com>
2022-09-12 14:02:06 +0100

    JS Data Types

---
+++ Data_Types/data_types.js
@@ -0,0 +1,39 @@
+/* 
+JavaScript data types are dynamic.
+A variable can be used to hold different data types
+*/
+
+
+// Numbers
+var x = 20;
+var y = 4.5;
+let z = 12.5e1; //z = 125
+
+
+// Strings
+let animal = "Wolf"; // double quotes
+let animal_color = 'White'; // single quotes
+
+
+// Booleans
+let t = true;
+let f = false;
+
+
+// Arrays
+const cars = ["Benz", "Toyota", "Honda"];
+const subjects = ["Mathematics", "English", "Computer Science"];
+
+
+// Objects
+const student = {
+    first_name : "Malli",
+    last_name : "Bones",
+    age : 22,
+};
+
+
+// Undefined
+let p;
+
+console.log(typeof(p));
