ec83fd05f0db05ec73aef8dad15123ced3fc2993
kadelcode <kadelcode@gmail.com>
2022-09-17 15:00:06 +0100

    JS Functions

---
+++ Functions/func.js
@@ -0,0 +1,17 @@
+/* JavaScript function is a block of code designed to perform a particular task.
+   You can use functions many times as you wish, once defined.
+*/
+
+
+function printName() {
+    console.log("Ayodele Kadiri");
+}
+
+// function with parameters
+function sum(a,b) {
+    return a + b; // The return statement ends the execution of the function
+}
+
+// executing the function
+printName();
+console.log(sum(5,6));
