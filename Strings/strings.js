4ee9d0e7315881d7ac0c7d40464e0f27a9c1d1d8
kadelcode <kadelcode@gmail.com>
2022-09-18 15:44:43 +0100

    Strings

---
+++ Strings/strings.js
@@ -0,0 +1,15 @@
+/* Strings are sequence of characters.
+   The are used for storing and manipulating text.
+*/
+
+
+let name = "Anya Pollo";
+
+let country = 'Poland';
+
+let text = "It's a new day!"
+console.log(text)
+
+// using the escape character
+let text1 = "It is a \"Volcano\""
+console.log(text1)
